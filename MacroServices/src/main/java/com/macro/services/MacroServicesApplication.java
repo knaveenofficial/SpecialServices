59fa33af375e6dc0228ce7964a415b5590383cef
user <user@192.168.0.103>
2017-03-18 22:01:00 +0530

    Services Intial Commit

---
+++ MacroServices/src/main/java/com/macro/services/MacroServicesApplication.java
@@ -0,0 +1,12 @@
+package com.macro.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacroServicesApplication.class, args);
+	}
+}
